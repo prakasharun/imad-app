f599429c1d1ba3a7fea8d6085f8317db2b2cd98f
prakasharun <30801020+prakasharun@users.noreply.github.com>
2017-08-08 20:39:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article one', function (req, res){
+app.get('/article one', function (req, res){
 res.sendfile('article one is dispalayes')});
 
-app.get('article two', function (req, res){
+app.get('/article two', function (req, res){
 res.sendfile('article one is dispalayes')});
 
-app.get('article three', function (req, res){
+app.get('/article three', function (req, res){
 res.sendfile('article one is dispalayes')});
 
 app.get('/ui/style.css', function (req, res) {
